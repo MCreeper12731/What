70dec637328454986bfc59b76d7ad9ae48aa126e
MCreeper12731 <vs97051@student.uni-lj.si>
2024-06-16 21:35:16 +0200

    Add project files.

---
+++ What/Instance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace What
+{
+    internal abstract class Instance
+    {
+        public char symbol;
+        public abstract String onHit();
+        public abstract String onTurn();
+    }
+}
